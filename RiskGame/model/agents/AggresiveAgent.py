3e44e582353c43351ddba50dfd544b855fb3cf45
Ahmed Hammad <hammad97official@gmail.com>
2018-11-25 03:14:02 +0200

    Done with Aggresive Agent + Pacifist Agent

--- RiskGame/model/agents/AggresiveAgent.py
+++ RiskGame/model/agents/AggresiveAgent.py
@@ -5,7 +5,32 @@ class AggresiveAgent:
         self.color = color
 
     def deploy(self, map, armies):
-        pass
+        maximum = 0
+        maxCity = None
+        for city in map:
+            if city.owner == self.color:
+                if city.armies > maximum:
+                    maximum = city.armies
+                    maxCity = city
+        if maxCity:
+            maxCity.armies += armies
+        return AgentsHelper.sendDeployments(self.color, armies)
+
 
     def attack(self, map):
-        pass
+        for city in map:
+            if city.owner == self.color:
+                maximum = 0
+                maxNeighbour = None
+                for neighbour in city.neighbours:
+                    if neighbour.armies < city.armies:
+                        if neighbour.armies > maximum:
+                            maximum = neighbour.armies
+                            maxNeighbour = neighbour
+
+                if maxNeighbour:
+                    neighbour.armies = city.armies - maximum - 1
+                    city.armies = 1
+                    neighbour.owner = self.color
+
+        return AgentsHelper.sendTroops(self.color)
--- RiskGame/model/agents/NPacifistAgent.py
+++ RiskGame/model/agents/NPacifistAgent.py
@@ -5,7 +5,40 @@ class NPacifistAgent:
         self.color = color
 
     def deploy(self, map, armies):
-        pass
+        #Find city with minimum armies
+        minimum = 999999
+        minCity = None
+        for city in map:
+            if city.owner == self.color:
+                if city.armies < minimum:
+                    minimum = city.armies
+                    minCity = city
+        if minCity:
+            minCity.armies += armies
+        return AgentsHelper.sendDeployments(self.color, armies)
 
     def attack(self, map):
-        pass
+        #Find oppenent city with minimum armies
+        minimum = 999999
+        minCity = None
+        for city in map:
+            if city.owner != self.color:
+                if city.armies < minimum:
+                    minimum = city.armies
+                    minCity = city
+        if minCity:
+            #Find city with maximum armies
+            strongestNumber = 0
+            strongestCity = None
+            for city in minCity.neighbours:
+                if city.armies > strongestNumber:
+                    strongestNumber = city.armies
+                    strongestCity = city
+
+            #Check if it can attack
+            if strongestNumber>minimum:
+                strongestCity.armies = 1
+                minCity.armies = strongestNumber - minimum - 1
+                minCity.owner = self.color
+
+            return AgentsHelper.sendTroops(self.color)
